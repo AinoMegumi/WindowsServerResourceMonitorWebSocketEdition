4ffd99beb904d662fe4a613f6f56ca23f2e69064
AinoMegumi <aimegu2014@gmail.com>
2020-05-03 23:12:10 +0900

    実行ユーザーをLocalServiceに変更

--- Server/ServiceInformation.h
+++ Server/ServiceInformation.h
@@ -3,14 +3,14 @@
 #include <tchar.h>
 
 namespace ServiceInfo {
-	constexpr TCHAR* Name = _T("ResMonSrvSvc");
-	constexpr TCHAR* DisplayName = _T("Windows Resource Monitor Server");
+	constexpr TCHAR* Name = _T("ResMonWebSockSrvSvc");
+	constexpr TCHAR* DisplayName = _T("Windows Resource Monitor Server Web Socket Edition");
 	constexpr TCHAR* Description = _T("Get computer resource with running Windows Server");
 	constexpr DWORD StartType = SERVICE_AUTO_START;
 	constexpr bool DelayedStart = true;
 	// LocalSystem    : NULL
 	// LocalService   : NT AUTHORITY\\LocalService
 	// NetworkService : NT AUTHORITY\\NetworkService
-	constexpr const TCHAR* ExecutionUser = NULL;
+	constexpr const TCHAR* ExecutionUser = TEXT("NT AUTHORITY\\LocalService");
 	constexpr bool RequireAdministrator = true;
 }
